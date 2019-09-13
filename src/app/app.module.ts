886576ef9dcd5d838d982b196395fcdce8cc3c49
leelavathikm1 <leelavathikm1@gmail.com>
2019-09-13 07:44:35 +0530

    add

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { AdminModule } from "./admin/admin.module";
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { StarterComponent } from "./starter/starter.component";
+import { StarterHeaderComponent } from "./starter/starter-header/starter-header.component";
+import { StarterLeftSideComponent } from "./starter/starter-left-side/starter-left-side.component";
+import { StarterContentComponent } from "./starter/starter-content/starter-content.component";
+import { StarterFooterComponent } from "./starter/starter-footer/starter-footer.component";
+import { StarterControlSidebarComponent } from "./starter/starter-control-sidebar/starter-control-sidebar.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StarterComponent,
+    StarterHeaderComponent,
+    StarterLeftSideComponent,
+    StarterContentComponent,
+    StarterFooterComponent,
+    StarterControlSidebarComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, AdminModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
